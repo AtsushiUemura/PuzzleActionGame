31feff94319d18e888cf274f724b47db5d932682
AtsushiUemura <atsushi.git@gmail.com>
2017-02-17 04:26:12 +0900

    追加：AudioManager、SingletonMonoBehaviour、Log

---
+++ Assets/Scripts/Log.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+public class Log {
+
+    private static string path = "./";
+    private static string fileName = "Log";
+    private static bool addWritable = true;
+    private static string encoding = "shift_jis";
+
+    private static List<string> LogList = new List<string>();
+
+    /// <summary>
+    /// Listクリア
+    /// </summary>
+    private static void Clear() {
+        LogList.Clear();
+    }
+    /// <summary>
+    /// List表示
+    /// </summary>
+    private static void Display() {
+        foreach (var item in LogList) {
+            Debug.Log(item);
+        }
+    }
+    /// <summary>
+    /// 書き込み
+    /// </summary>
+    /// <param name="log"></param>
+    public static void Write(string log) {
+        StreamWriter sw = new StreamWriter(path + fileName, addWritable, Encoding.GetEncoding(encoding));
+        sw.Write("CALLED: " + log + "\n");
+        sw.Close();
+    }
+    /// <summary>
+    /// 読み込み
+    /// </summary>
+    public static void Read() {
+        Clear();
+        StreamReader sr = new StreamReader(path + fileName, Encoding.GetEncoding(encoding));
+        Debug.Log(sr.Peek());
+        while (sr.Peek() >= 0) {
+            string tmp = sr.ReadLine();
+            LogList.Add(tmp);
+        }
+        sr.Close();
+        Display();
+    }
+
+}
---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class AudioManager : MonoBehaviour {
+
+    [SerializeField]
+    private AudioSource SEAudioSource;
+    [SerializeField]
+    private AudioSource BGMAudioSource;
+
+    public AudioClip BGM = null;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void PlaySE(AudioClip se) {
+        Log.Write(MethodBase.GetCurrentMethod().ToString());
+        SEAudioSource.PlayOneShot(se);
+    }
+
+    public void PlayBGM(AudioClip bgm) {
+        Log.Write(MethodBase.GetCurrentMethod().ToString());
+        BGMAudioSource.clip = bgm;
+        BGMAudioSource.Play();
+    }
+}
